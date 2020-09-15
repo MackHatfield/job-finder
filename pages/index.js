199953dd7cbd2e83df9e0e143d1c19111d8d4165
Mackenzie Hatfield <therealbakoonzar@gmail.com>
2020-09-15 16:52:43 -0700

    Setup basic home page.

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,47 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styled from "styled-components";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    <PageContainer>
+      <ContentContainer>
+        <PageTitle>Job Finder</PageTitle>
+        <Caption>Find Awesome Jobs Today!</Caption>
+        <Button>CHECK IT OUT</Button>
+      </ContentContainer>
+    </PageContainer>
+  );
 }
+
+const PageContainer = styled.div`
+  width: 100%;
+  display: flex;
+  background-color: pink;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto;
+  height: 600px;
+`
+const ContentContainer = styled.div`
+  flex-direction: row;
+  text-align: center;
+  justify-content: center;
+`;
+
+const PageTitle = styled.h1`
+  font-weight: 600;
+  font-size: 4.5em;
+  letter-spacing: .25em;
+  text-decoration: underline #1cd860;
+`;
+
+const Caption = styled.h3`
+  font-size: 1.75em;
+`;
+
+const Button = styled.button`
+  background-color: #3db6f2;
+  border: 2px solid #3db6f2;
+  color: white;
+  border-radius: 3px;
+  padding: 10px 40px 10px 40px;
+`;
