2d67e3f720a840984ede86aa01056f6e7b735a60
Mackenzie Hatfield <therealbakoonzar@gmail.com>
2020-09-19 15:55:41 -0700

    Make Container height full viewport size.

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,17 @@
 import styled from 'styled-components';
 
-const Layout = props => {
-  <Container>
-    <Content>{props.children}</Content>
-  </Container>
+const Layout = (props) => {
+  return (
+    <Container>
+      <Content>{props.children}</Content>
+    </Container>
+  );
 };
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100%;
+  height: 100vh;
   width: 100%;
 `;
 
