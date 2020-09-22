c5e249c6a0e448a55303754ff7c46bd2295880d2
Mackenzie Hatfield <therealbakoonzar@gmail.com>
2020-09-21 21:21:29 -0700

    Add stub for Search component.

---
+++ components/Search.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Search = props => {
+  return <Container />
+}
+
+export default Search;
+
+const Container = styled.div`
+  width: 100%;
+  height: 200px;
+  border-bottom: 1px solid purple;
+`;
