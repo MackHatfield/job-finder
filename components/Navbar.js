879a8bfcd079e0b656d780885a8fe7c87c64ab61
Mackenzie Hatfield <therealbakoonzar@gmail.com>
2020-09-21 21:20:50 -0700

    Add hover effects and selected page effect.

---
+++ components/Navbar.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+import Link from 'next/link';
+
+const Navbar = ({ currentPage }) => {
+  console.log(currentPage);
+  return (
+    <Container>
+      <AppTitle>Job Finder</AppTitle>
+      <LinkContainer>
+        <Link href='/'><LinkText selected={currentPage === 'home'}>Home</LinkText></Link>
+        <Link href='/jobs'><LinkText selected={currentPage === 'jobs'}>Job Search</LinkText></Link>
+      </LinkContainer>
+      
+    </Container>
+  )
+};
+
+const Container = styled.div`
+  width: 100%;
+  background-color: white;
+  justify-content: center;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  padding-left: 5px;
+  background-color: #e8e8e8;
+  border-bottom: 2px solid #d6d4d4;
+`;
+
+const AppTitle = styled.h2`
+  display: inline;
+  text-decoration: underline #1cd860;
+`;
+
+const LinkContainer = styled.span`
+  margin-left: 20px;
+`;
+
+const LinkText = styled.a`
+  margin-right: 10px;
+  color: ${props => props.selected ? '#1cd860' : 'black'};
+  &:hover {
+    ${LinkText} {
+      text-shadow: 1px 1px 2px gray;
+    }
+  }
+`;
+
+export default Navbar;
